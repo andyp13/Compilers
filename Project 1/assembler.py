fc6024650d5514eae30fb825ce9e3f6b8fc9c346
Aaron <aarona@phillipz.me>
2020-03-20 13:51:45 -0600

    Fixed the assembler

--- Project 1/assembler.py
+++ Project 1/assembler.py
@@ -10,25 +10,25 @@ def representsInt(myInt : int):
     except:
         return False
 def representsHex(myInt: str):
-    try:
-        if(myInt.lower().startswith('0x')):
-            myInt = myInt[2:]
+    if(myInt.lower().startswith('0x')):
+        myInt = myInt[2:]
+        try:
             int(myInt, 16)
             return True
-        else:
-            return False
-    except:
+        except:
+            raise ValueError
+    else:
         return False
 
 def representsBin(myInt: str):
-    try:
-        if(myInt.lower().startswith('0b')):
-            myInt = myInt[2:]
+    if(myInt.lower().startswith('0b')):
+        myInt = myInt[2:]
+        try:
             int(myInt, 2)
             return True
-        else:
-            return False
-    except:
+        except:
+            raise ValueError
+    else:
         return False
 
 # Simple class to hold label data
@@ -44,12 +44,21 @@ class Label:
         else:
             return self.name == other.name
 
+class Log:
+    def __init__(self, fileName):
+        self.filename = fileName + '.out'
+    def log(self, string):
+        print(string)
+        f = open(self.filename, 'a')
+        f.write(str(string) + '\n')
+
+
 
 #Main Assembler
 class hackAssembler:
     
 
-    def __init__(self):
+    def __init__(self, logger):
         self.currentLineNumber = 0
         self.currentLabelNumber = 16
         # Holders for the label classes (Makes it easy to print)
@@ -57,6 +66,7 @@ class hackAssembler:
         self.RAMLabels = []
         self.ROMLabels = []
         self.EQULabels = []
+        self.logger = logger
 
         # Dictionary to all the EQU symbold
         self.equLabelsDict = {
@@ -136,7 +146,23 @@ class hackAssembler:
             "!(D|A)": 0b0010100,
             "!(!D|A)": 0b0000100,
             "!(D|!A)": 0b0010000,
-            "!(!D|!A)": 0b000000
+            "!(!D|!A)": 0b000000,
+
+            "!D&M"  : 0b1010000,
+            "D&!M"  : 0b1000100,
+            "!D&!M" : 0b1010100,
+            "!(D&M)": 0b1000001,
+            "!(!D&M)": 0b1010001,
+            "!(D&!M)": 0b1000101,
+            "!(!D&!M)": 0b1010101,
+
+            "!D|M"  : 0b1000101,
+            "D|!M"  : 0b1010001,
+            "!D|!M" : 0b1000001,
+            "!(D|M)": 0b1010100,
+            "!(!D|M)": 0b1000100,
+            "!(D|!M)": 0b1010000,
+            "!(!D|!M)": 0b100000
         }
 
         #All destination conversions
@@ -169,8 +195,29 @@ class hackAssembler:
     # Starts the entire assembler
     def assembler(self, currentFile: str):
         totalFile = ""
+        inComment = False
         for index, currentLine in enumerate(currentFile):
             currentLine = currentLine.strip()
+            if not inComment:
+                commentStart = currentLine.find('/*')
+                commentEnd = currentLine.find('*/')
+                if(commentStart != -1):
+                    if(commentEnd != -1):
+                        inComment = True
+                        continue
+                    else:
+                        currentLine = currentLine[:commentStart] + currentLine[commentEnd:]
+            else:
+                commentEnd = currentLine.find('*/')
+                currentLine = currentLine[commentEnd:]
+                inComment = False
+
+            lineComment = currentLine.find('//')
+            if(lineComment != -1):
+                currentLine = currentLine[:lineComment]
+
+            currentLine.strip()
+
             try: 
                 if(len(currentLine)):
                     # Do Stuff Here
@@ -185,57 +232,56 @@ class hackAssembler:
                     elif(currentLine.startswith('@') or currentLine.startswith('(')):
                         #Looking at an A Type
                         newLine = self.aType(index, currentLine)
-                    elif(currentLine.lower().startswith('x')):
-                        # Looking at an X Type
-                        newLine = self.xType(index, currentLine)
                     else:
                         # We are looking at a c type instruction
                         newLine =  self.cType(index, currentLine)
                     if(newLine != None): # Must have been a comment or a blank line
                             totalFile += newLine + "\n"
             except Exception as ex:
-                print(ex.args)
+                self.logger.log(ex.args)
                 number, error, line = ex.args if (len(ex.args) == 3) else ex.args, None, None
-                print(f'ERROR - On line {number}\n{error}\n{line}', file=sys.stderr)
+                self.logger.log(f'ERROR - On line {number}\n{error}\n{line}')
         return totalFile
             
     def aType(self, lineNumber, line : str):
         AInst = line.strip('@')
+        AInst = AInst.strip()
         if AInst.startswith('('):
             return None
         if(len(AInst) == 0): # Make sure we are not looking at a blank line
             raise Exception(lineNumber, "A-Type Illegal Value: Not a Number", line)
 
         # Figure out what Kinda number or text we are looking at
-        if(representsInt(AInst) or representsHex(AInst) or representsBin(AInst)):
-            location = 0
-            if(representsInt(AInst)):
-                location = int(AInst)
-            elif(representsHex(AInst)):
-                location = int(AInst[2:], 16)
+        try:
+            if(representsInt(AInst) or representsHex(AInst) or representsBin(AInst)):
+                location = 0
+                if(representsInt(AInst)):
+                    location = int(AInst)
+                elif(representsHex(AInst)):
+                    location = int(AInst[2:], 16)
+                else:
+                    location = int(AInst[2:], 2)
+                if(location < 0):
+                    raise Exception(lineNumber, "A-Type Illegal Value: Number cannot be Negative", line)
+                if(location > 0b111111111111111):
+                    raise Exception(lineNumber, "A-Type Illegal Value: Number is larger than a 15 bit number", line)
+                location &= ~(1 << 15)
+                locationText = bin(location)[2:].zfill(16)  # Convert to binary text
+                return locationText
             else:
-                location = int(AInst[2:], 2)
-            if(location < 0):
-                raise Exception(lineNumber, "A-Type Illegal Value: Number cannot be Negative", line)
-            if(location > 0b111111111111111):
-                raise Exception(lineNumber, "A-Type Illegal Value: Number is larger than a 15 bit number", line)
-            location &= ~(1 << 15)
-            locationText = bin(location)[2:].zfill(16)  # Convert to binary text
-            return locationText
-        else:
-            if(self.labels.get(AInst) is not None):     # If we already have the label, say it 
-                return bin(self.labels.get(AInst))[2:].zfill(16)
-            else:
-                raise Exception(lineNumber, "A-Type No Value", line)
+                if(self.labels.get(AInst) is not None):     # If we already have the label, say it 
+                    return bin(self.labels.get(AInst))[2:].zfill(16)
+                else:
+                    raise Exception(lineNumber, "A-Type No Value", line)
+        except ValueError:
+            raise Exception(lineNumber, "Value is not a number", line)
 
     def xType(self, lineNumber, line:str):
-        cDefinition = 0xe000
         if(representsHex('0'+ line)): # Make sure it is hex and convert it to an int
             command = int(line[1:], 16)
             if(command > 127):
                 raise Exception(lineNumber, "x Type Instruction is greater than the number of commands", line)
-            output = cDefinition | command
-            return bin(output)[2:].zfill(16)  # Convert to binary string
+            return command  # Convert to binary string
         else:
             raise Exception(lineNumber, "X Type Instuction is not Hexadecimal", line)
 
@@ -243,14 +289,20 @@ class hackAssembler:
         cDefinition = 0xe000    # Base C Type Intruction
 
         # Find All the different Parts
-        destText = line.split('=')[0] if '=' in line else None
-        jumpText = line.split(';')[1] if ';' in line else None
+        destText = line.split('=')[0].strip() if '=' in line else None
+        jumpText = line.split(';')[1].strip() if ';' in line else None
         destIndex = line.find('=')
         jumpIndex = line.find(';')
-        compText = line[destIndex + 1 if destIndex != -1 else 0 : jumpIndex if jumpIndex != -1 else len(line)]
+        compText = line[destIndex + 1 if destIndex != -1 else 0 : jumpIndex if jumpIndex != -1 else len(line)].strip()
+
+        comp = int()
+        if(compText.lower().startswith('x')):
+            comp = self.xType(lineNumber, compText)
+        else:
+            comp = self.compConversion.get(compText)
 
         # Convert Text to int
-        comp = self.compConversion.get(compText)
+        
         if(comp is None):
             raise Exception(lineNumber, "Illegal C-Type computation: Not a valid Computation Mnemonic", line)
         dest = self.destConversion.get(destText)
@@ -288,45 +340,63 @@ class hackAssembler:
         # Does the first run through to find all the labels. Adds them to their respective buckets
         for key in self.labels: # Adds the inital ones as well
             self.RAMLabels.append(Label(key, self.labels.get(key), -1))
-        
+        inComment = False
         for lineNumber, currentLine in enumerate(currentFile):
             try: 
                 currentLine = currentLine.strip()
+                if not inComment:
+                    commentStart = currentLine.find('/*')
+                    commentEnd = currentLine.find('*/')
+                    if(commentStart != -1):
+                        if(commentEnd != -1):
+                            currentLine = currentLine[:commentStart] + currentLine[commentEnd+2:]
+                        else:  
+                            inComment = True
+                            continue
+                else:
+                    commentEnd = currentLine.find('*/')
+                    currentLine = currentLine[commentEnd:]
+                    inComment = False
+                currentLine.strip()
+
                 label = currentLine.strip('@')
                 label = label.strip()
                 if(len(label) == 0 and currentLine.startswith('@')):
                     raise Exception(lineNumber, "Illegal Label: Label cannot be blank", currentLine)
 
-                if(currentLine.startswith('@') and not representsInt(label)):
-                    newLabel = Label(label, 0, lineNumber)
-                    if(self.labels.get(label) == None and (newLabel not in self.currentMemory)):
-                        self.currentMemory.append(newLabel)
+                try:
+                    if(currentLine.startswith('@') and not (representsInt(label) or representsHex(label) or representsBin(label))):
+                        newLabel = Label(label, 0, lineNumber)
+                        if(self.labels.get(label) == None and (newLabel not in self.currentMemory)):
+                            self.currentMemory.append(newLabel)
 
-                elif(currentLine.startswith('(')):
-                    label = currentLine.strip("(")
-                    label = label.replace(")", "")
-                    label = label.strip()
-                    if(label in self.currentMemory):
-                        self.currentMemory.remove(label)
-                    if(self.labels.get(label) != None):
-                        raise Exception(lineNumber, "Illegal Label Redefinition", currentLine)
-                    self.labels[label] = self.currentLineNumber
-                    self.ROMLabels.append(Label(label, self.currentLineNumber, lineNumber))
+                    elif(currentLine.startswith('(')):
+                        label = currentLine.strip("(")
+                        label = label.replace(")", "")
+                        label = label.strip()
+                        if(label in self.currentMemory):
+                            self.currentMemory.remove(label)
+                        if(self.labels.get(label) != None):
+                            raise Exception(lineNumber, "Illegal Label Redefinition", currentLine)
+                        self.labels[label] = self.currentLineNumber
+                        self.ROMLabels.append(Label(label, self.currentLineNumber, lineNumber))
 
-                elif(currentLine.startswith('.EQU ')):
-                    try:
-                        splitLine = currentLine.split(' ')
-                        self.EQULabels.append(Label(splitLine[1], splitLine[2], lineNumber))
-                        self.equLabelsDict[splitLine[1]] = splitLine[2]
-                    except:
-                        raise Exception(lineNumber, "Illegal EQU: EQU not formatted Correctly", currentLine)
-                if(self.validInstruction(currentLine) == True):
-                    self.currentLineNumber += 1
+                    elif(currentLine.startswith('.EQU ')):
+                        try:
+                            splitLine = currentLine.split(' ')
+                            self.EQULabels.append(Label(splitLine[1], splitLine[2], lineNumber))
+                            self.equLabelsDict[splitLine[1]] = splitLine[2]
+                        except:
+                            raise Exception(lineNumber, "Illegal EQU: EQU not formatted Correctly", currentLine)
+                    if(self.validInstruction(currentLine) == True):
+                        self.currentLineNumber += 1
 
+                except ValueError:
+                    raise Exception(lineNumber, "Value is not a number", currentLine)
             except Exception as ex:
-                print(ex.args)
+                self.logger.log(ex.args)
                 number, error, line = ex.args if len(ex.args) == 3 else (ex.args[0], None, None)
-                print(f'ERROR - On line {number}\n{error}\n{line}', file=sys.stderr)
+                self.logger.log(f'ERROR - On line {number}\n{error}\n{line}')
 
         for items in self.currentMemory:
             self.labels[items.name] = self.currentLabelNumber
@@ -340,7 +410,9 @@ if(len(sys.argv) < 2):
 
 
 for filePath in sys.argv[1:]:
-    assembler = hackAssembler()
+    fileName = filePath.split(".")[0]
+    logger = Log(fileName)
+    assembler = hackAssembler(logger)
     file = open(filePath)
 
     if(file.mode != 'r'):
@@ -351,12 +423,12 @@ for filePath in sys.argv[1:]:
     assembler.getLabels(currentFile)
     # Compile Program
     totalFile = assembler.assembler(currentFile)
-    fileName = filePath.split(".")[0]
+
     newFileName = fileName+ ".hack"
     newFile = open(newFileName, "w")
     newFile.write(totalFile)
     newFile.close()
-    print(f'Outputed to -> {newFileName}')
+    logger.log(f'Outputed to -> {newFileName}')
 
     tableFile = ''
     tableFile += ('Symbolic Table Time!\nRAM Addresses:\n')
@@ -379,8 +451,8 @@ for filePath in sys.argv[1:]:
     newTableFile = open(tableFileName, 'w')
     newTableFile.write(tableFile)
     newTableFile.close()
-    print(tableFile)
-    print(f'Also Written To -> {tableFileName}')
+    logger.log(tableFile)
+    logger.log(f'Also Written To -> {tableFileName}')
 
     
     
